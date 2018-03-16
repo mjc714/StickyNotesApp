1e4ea07b7a28a05bf2cfb85c1e46cd40ef9a607a
Matthew Chin <chin.matthew1212@gmail.com>
2018-03-15 23:41:57 -0400

    Created todo form. Scaffolded Todos Controller.

--- StickyNotesApp/Data/ApplicationDbContext.cs
+++ StickyNotesApp/Data/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using StickyNotesApp.Models;
 
@@ -15,12 +11,15 @@ namespace StickyNotesApp.Data
         {
         }
 
+        public DbSet<Todo> Todos { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+            builder.Entity<Todo>().ToTable("Todo");
         }
     }
 }
---
+++ StickyNotesApp/Models/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StickyNotesApp.Models
+{
+    public class Todo
+    {
+        // ID of a Todo.
+        public int ID { get; set; }
+
+        // user ID from AspNetUser table.
+        public string OwnderID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsDone { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Expire Date")]
+        public DateTime? ExprireDate { get; set; }
+    }
+}
